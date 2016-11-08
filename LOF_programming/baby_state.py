4eeea2c5a303a5d2bafecbdebfda1cf5e87a86b5
GameForPeople <Koreagamemaker@gmail.com>
2016-11-08 22:49:57 +0900

    2016-11-08 baby_statue 시작

---
+++ LOF_programming/baby_state.py
@@ -0,0 +1,37 @@
+
+from pico2d import *
+
+name = "StartState"
+
+def enter():
+    open_canvas()
+
+
+def exit():
+
+    close_canvas()
+
+
+def update():
+  pass
+
+def draw():
+
+    clear_canvas()
+
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    pass
+
+
+def pause(): pass
+
+
+def resume(): pass
+
+
+
+
